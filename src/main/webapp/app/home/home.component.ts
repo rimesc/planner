68600bbcf72801f60ded46b6a8ffaf3b1670e3a0
Chris Rimes <chris.rimes@gmail.com>
2019-03-10 22:50:34 +0000

    Display a grid of themes on the home page.
    For now this includes the last 6 themes created, with no concern for owner or visibility.

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { JhiEventManager } from 'ng-jhipster';
+import { JhiEventManager, JhiAlertService, JhiDataUtils } from 'ng-jhipster';
 
 import { LoginModalService, AccountService, Account } from 'app/core';
+import { ThemeService } from 'app/entities/theme';
+import { ITheme } from 'app/shared/model/theme.model';
 
 @Component({
     selector: 'jhi-home',
@@ -12,16 +17,25 @@ import { LoginModalService, AccountService, Account } from 'app/core';
 export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
+    themes: ITheme[];
+    maxCards: number;
 
     constructor(
         private accountService: AccountService,
         private loginModalService: LoginModalService,
+        private themeService: ThemeService,
+        private jhiAlertService: JhiAlertService,
+        protected dataUtils: JhiDataUtils,
         private eventManager: JhiEventManager
-    ) {}
+    ) {
+        this.themes = [];
+        this.maxCards = 6; // ideally a multiple of 6 as there are 1, 2 or 3 columns depending on screen size
+    }
 
     ngOnInit() {
         this.accountService.identity().then((account: Account) => {
             this.account = account;
+            this.loadThemes();
         });
         this.registerAuthenticationSuccess();
     }
@@ -30,6 +44,7 @@ export class HomeComponent implements OnInit {
         this.eventManager.subscribe('authenticationSuccess', message => {
             this.accountService.identity().then(account => {
                 this.account = account;
+                this.loadThemes();
             });
         });
     }
@@ -40,5 +55,19 @@ export class HomeComponent implements OnInit {
 
     login() {
         this.modalRef = this.loginModalService.open();
+    }
+
+    loadThemes() {
+        this.themeService
+            .query({
+                page: 0,
+                size: 6,
+                sort: ['id,asc']
+            })
+            .subscribe((res: HttpResponse<ITheme[]>) => (this.themes = res.body), (res: HttpErrorResponse) => this.onError(res.message));
+    }
+
+    protected onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
     }
 }
---
+++ src/test/javascript/spec/app/home/home.component.spec.ts
@@ -0,0 +1,51 @@
+/* tslint:disable max-line-length */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Observable, of } from 'rxjs';
+import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { ActivatedRoute, Data } from '@angular/router';
+
+import { PlannerTestModule } from '../../test.module';
+import { HomeComponent } from 'app/home/home.component';
+import { ThemeService } from 'app/entities/theme/theme.service';
+import { Theme } from 'app/shared/model/theme.model';
+
+describe('Component Tests', () => {
+    describe('Home Component', () => {
+        let comp: HomeComponent;
+        let fixture: ComponentFixture<HomeComponent>;
+        let service: ThemeService;
+
+        beforeEach(() => {
+            TestBed.configureTestingModule({
+                imports: [PlannerTestModule],
+                declarations: [HomeComponent]
+            })
+                .overrideTemplate(HomeComponent, '')
+                .compileComponents();
+
+            fixture = TestBed.createComponent(HomeComponent);
+            comp = fixture.componentInstance;
+            service = fixture.debugElement.injector.get(ThemeService);
+        });
+
+        it('should load top themes', () => {
+            // GIVEN
+            const headers = new HttpHeaders().append('link', 'link;link');
+            spyOn(service, 'query').and.returnValue(
+                of(
+                    new HttpResponse({
+                        body: [new Theme(123)],
+                        headers
+                    })
+                )
+            );
+
+            // WHEN
+            comp.loadThemes();
+
+            // THEN
+            expect(service.query).toHaveBeenCalledWith({ page: 0, size: 6, sort: ['id,asc'] });
+            expect(comp.themes[0]).toEqual(jasmine.objectContaining({ id: 123 }));
+        });
+    });
+});
